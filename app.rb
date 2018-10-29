02cba3a2d17d2fe9dc3c347a6d800f10d54b628b
github username <github email address>
2018-10-29 15:30:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,45 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get "/reversename/:name" do
+    @name = params[:name]
+    @name.reverse
+  end
+
+  get "/square/:number" do
+    @number = params[:number].to_i
+    "#{@number * @number}"
+  end
+
+  get "/say/:number/:phrase" do
+    @phrase = params[:phrase].to_s
+    @number = params[:number].to_i
+    result = ""
+    @number.times do result += "#{@phrase} "
+    end
+    result
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    if params[:operation] == "add"
+       "#{params[:number1].to_i + params[:number2].to_i}"
+     elsif params[:operation] == "subtract"
+       "#{params[:number1].to_i - params[:number2].to_i}"
+     elsif params[:operation] == "multiply"
+       "#{params[:number1].to_i * params[:number2].to_i}"
+     elsif params[:operation] == "divide"
+       "#{params[:number1].to_i / params[:number2].to_i}"
+     end
+  end
 
 end
